c00ca418c76de7f0b6590f797af8fc2fc138c8ac
Hugo Roque <hugolnx@gmail.com>
2016-06-12 11:29:25 -0300

    implementing LRU

--- simulador.c
+++ simulador.c
@@ -9,13 +9,13 @@ typedef struct stTableElement {
 	short lastAccess;
   char isReferenced;
   char isModified;
-} tpElem;
+} tpPage;
 
 char *substAlg;
 int pageSize, memSize;
 int pageSizeBitsBoundery;
 unsigned int instructions[500][2]; // [adress, operation (0 - read 1 - write]
-tpElem pageTable[600000]; // [last access, referenced?, modified?]
+tpPage pageTable[600000];
 int referencedCount = 0;
 int time = 0;
 int writingCount = 0;
@@ -34,20 +34,47 @@ void readInstructions(char *path) {
 	DEBUG(printf("%d instructions readed\n<===\n\n\n", instructions[0][0]);)
 }
 
-void resetPage(unsigned int logicalAddress) {
-	memset(&pageTable[logicalAddress], 0x00, sizeof(tpElem));
+tpPage *nextPageToBeReplaced() {
+	unsigned int i;
+	if(strcmp(substAlg, "lru") == 0) {
+		tpPage *leastRecent = NULL;
+		for(i = 0; i < 600000; i++) {
+			if(pageTable[i].isReferenced &&
+					(leastRecent == NULL ||
+					 pageTable[i].lastAccess < leastRecent->lastAccess)) {
+				leastRecent = pageTable + i;
+			}
+		}
+		return leastRecent;
+	} else if(strcmp(substAlg, "nru") == 0) {
+	} else if(strcmp(substAlg, "seg") == 0) {
+	}
+	printf("Choose one of those page replace algorithm: LRU, NRE, SEG");
+	exit(1);
+}
+
+char *uniformizeAlgorithmName(char *name) {
+	int i;
+	for(i = 0; i < strlen(name); i++) {
+		name[i] = tolower(name[i]);
+	}
+	return name;
+}
+
+void resetPage(tpPage *page) {
+	memset(page, 0x00, sizeof(tpPage));
 }
 
 int main(int argc, char *argv[])
 {
 	int i;	
-	substAlg = argv[1];
+	substAlg = uniformizeAlgorithmName(argv[1]);
 	readInstructions(argv[2]);
 	pageSize = atoi(argv[3]);
 	memSize = atoi(argv[4]);
 	pageSizeBitsBoundery = (int) ceil(log2(pageSize*1024));
 
-	memset(pageTable, 0x00, 600000*sizeof(tpElem));
+	memset(pageTable, 0x00, 600000*sizeof(tpPage));
 	for(i = 1; i <= instructions[0][0]; i++) {
 		unsigned int address = instructions[i][0];
 		unsigned int isWriting = instructions[i][2];
@@ -59,10 +86,13 @@ int main(int argc, char *argv[])
 		if((referencedCount+1)*pageSize > memSize) {
 			// Use one of the methods to replace page
 			pageFaultCount++;
-			/*
-			 * unsigned int logicalAddrOfPageToBeDeleted = pageToBeDeleted(); // using the right method
-			 * resetPage(logicalAddrOfPageToBeDeleted);
-			 */
+			
+			tpPage *pageToBeReplaced = nextPageToBeReplaced();
+			if(pageToBeReplaced != NULL) {
+				resetPage(pageToBeReplaced);
+				referencedCount--;
+			}
+			
 		}
 
 		pageTable[logicalAddress].lastAccess = time;
