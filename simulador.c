f4669208549e694df5681edcf392162c7a595e56
dmandarino <douglasmandarino@Douglass-MacBook-Pro.local>
2016-06-14 23:55:05 -0300

    colocando o seg

--- simulador.c
+++ simulador.c
@@ -3,7 +3,7 @@
 #include <math.h>
 #include <string.h>
 
-#define DEBUG(A...) printf("<DEBUG> "); printf(A)
+#define DEBUG(A...)// printf("<DEBUG> "); printf(A)
 #define RECENT_DELTA_TIME 100
 
 typedef struct stTableElement {
@@ -11,12 +11,20 @@ typedef struct stTableElement {
   char isReferenced;
   char isModified;
   char isPresent;
+  char isReferencedSeg;
 } tpPage;
+
+typedef struct circularQueue {
+    tpPage *pages[3000];
+    int begin;
+    int end;
+} queue;
 
 char *substAlg;
 int pageSize, memSize;
 int pageSizeBitsBoundery;
 tpPage pageTable[600000];
+queue Q;
 int presentCount = 0;
 int time = 0;
 int writingCount = 0;
@@ -35,6 +43,11 @@ tpPage *lruAlgorithm() {
 	return leastRecent;
 }
 
+void append (tpPage *page) {
+    Q.end = (Q.end + 1) % 3000;
+    Q.pages[Q.end] = page;
+}
+
 tpPage *nruAlgorithm() {
 	unsigned int i;
 	int maxPoints;
@@ -50,12 +63,24 @@ tpPage *nruAlgorithm() {
 	return choosed;
 }
 
+tpPage *secAlgorithm() {
+    tpPage* first = Q.pages[Q.begin];
+    Q.begin = (Q.begin + 1) % 3000;
+    if(first->isReferencedSeg == 1) {
+        first->isReferencedSeg = 0;
+        append(first);
+        return secAlgorithm();
+    }
+    return first;
+}
+
 tpPage *nextPageToBeReplaced() {
 	if(strcmp(substAlg, "lru") == 0) {
 		return lruAlgorithm();
 	} else if(strcmp(substAlg, "nru") == 0) {
 		return nruAlgorithm();
 	} else if(strcmp(substAlg, "seg") == 0) {
+        return secAlgorithm();
 	}
 	printf("Choose one of those page replace algorithm: LRU, NRE, SEG");
 	exit(1);
@@ -102,14 +127,17 @@ void executeInstruction(unsigned int address, int isWriting) {
 	DEBUG("%s address %x (logical: %x) - time: %d\n", (isWriting ? "Writing on" : "Reading"), address, logicalAddress, time);
 
 	pageTable[logicalAddress].lastAccess = time;
+    
 	if(pageTable[logicalAddress].isPresent) {
 		DEBUG("Page %x are already in memory\n", logicalAddress);
 	} else {
 		DEBUG("Page %x are not in memory\n", logicalAddress);
 
 		verifyAndTreatPageFault(logicalAddress);
+        append(&pageTable[logicalAddress]);
 		pageTable[logicalAddress].isPresent = 1;
 		pageTable[logicalAddress].isReferenced = 1;
+        pageTable[logicalAddress].isReferencedSeg = 1;
 	}
 
 	if(isWriting) {
@@ -123,9 +151,10 @@ void executeInstruction(unsigned int address, int isWriting) {
 
 int main(int argc, char *argv[])
 {
-	int i;
 	unsigned int address;
 	char operation;
+    Q.begin = 0;
+    Q.end = -1;
 
 	printf("Executando o simulador...\n");
 
